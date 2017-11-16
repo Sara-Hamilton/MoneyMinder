859939a2472c36f38a6d10d916b68453bed7d89c
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2017-11-15 18:10:47 -0800

    add account controller

--- src/main/java/com/example/MoneyMinder/controllers/AccountController.java
+++ src/main/java/com/example/MoneyMinder/controllers/AccountController.java
@@ -1,14 +1,18 @@
 package com.example.MoneyMinder.controllers;
 
+import com.example.MoneyMinder.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
+@RequestMapping("account")
 public class AccountController {
 
-    @RequestMapping(value="")
+    /*@RequestMapping(value="")
     @ResponseBody
     public String index() {
         return "Hello World!";
@@ -18,6 +22,15 @@ public class AccountController {
     @ResponseBody
     public String goodbye() {
         return "Goodbye!";
+    } */
+
+    @RequestMapping(value = "")
+    public String index(Model model, HttpServletRequest request) {
+
+        User user = (User) request.getSession().getAttribute("user");
+        model.addAttribute("title", "Welcome test title for account index");
+        model.addAttribute("user", user);
+        return "account/index";
     }
 
 
