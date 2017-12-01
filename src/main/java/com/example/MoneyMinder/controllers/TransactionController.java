a70304542311d076ef2237d749b8b0f7e3f22416
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2017-12-01 10:57:51 -0800

    add ability to transfer between accounts

--- src/main/java/com/example/MoneyMinder/controllers/TransactionController.java
+++ src/main/java/com/example/MoneyMinder/controllers/TransactionController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -96,6 +97,87 @@ public class TransactionController {
 
         accountDao.save(account);
         userDao.save(user);
+
+        model.addAttribute("title", "Transaction Successful");
+        return "transaction/transaction-confirmation";
+    }
+
+    @RequestMapping(value = "transfer", method = RequestMethod.GET)
+    public String displayTransferForm(Model model, HttpServletRequest request) {
+
+        User user = (User) request.getSession().getAttribute("user");
+        List<Category> userCategories = categoryDao.findByUserId(user.getId());
+        List<Account> userAccounts = accountDao.findByUserId(user.getId());
+        model.addAttribute("userAccounts", userAccounts);
+        model.addAttribute("userCategories", userCategories);
+        model.addAttribute(new Transaction());
+        model.addAttribute("title", "Transfer Between Accounts");
+
+        return "transaction/transfer";
+    }
+
+    @RequestMapping(value="transfer", method = RequestMethod.POST)
+    public String processTransferForm(Model model, @ModelAttribute @Valid Transaction transaction,
+                                            Errors errors, @RequestParam int categoryId, @RequestParam int fromAccountId,
+                                            @RequestParam int toAccountId, HttpServletRequest request) {
+
+        User user = (User) request.getSession().getAttribute("user");
+
+        if (errors.hasErrors()) {
+            List<Category> userCategories = categoryDao.findByUserId(user.getId());
+            List<Account> userAccounts = accountDao.findByUserId(user.getId());
+            model.addAttribute("userAccounts", userAccounts);
+            model.addAttribute("userCategories", userCategories);
+            model.addAttribute(new Transaction());
+            model.addAttribute("title", "Errors");
+            return "transaction/transfer";
+        }
+
+        Category category = categoryDao.findOne(categoryId);
+        Account fromAccount = accountDao.findOne(fromAccountId);
+        Account toAccount = accountDao.findOne(toAccountId);
+
+        Date date = transaction.getDate();
+        String description = transaction.getDescription();
+
+        BigDecimal transactionAmount = transaction.getAmount();
+        BigDecimal fromTotal = fromAccount.getTotal();
+        BigDecimal toTotal = toAccount.getTotal();
+
+        // withdrawal from fromAccount
+        BigDecimal newFromTotal = fromTotal.subtract(transactionAmount);
+        fromAccount.setTotal(newFromTotal);
+
+        // save withdrawal transaction
+        transaction.setUser(user);
+        transaction.setType(TransactionType.WITHDRAWAL);
+        transaction.setAccount(fromAccount);
+        transaction.setCategory(category);
+        if(categoryId != 0) {transaction.setCategoryName(category.getName());}
+        transaction.setPreviousTotal(fromTotal);
+        transactionDao.save(transaction);
+        accountDao.save(fromAccount);
+
+
+        //create a new transaction for deposit into toAccount
+        Transaction toTransaction = new Transaction();
+
+        // deposit into toAccount
+        BigDecimal newToTotal = toTotal.add(transactionAmount);
+        toAccount.setTotal(newToTotal);
+
+        // save deposit transaction
+        toTransaction.setAmount(transactionAmount);
+        toTransaction.setDescription(description);
+        toTransaction.setDate(date);
+        toTransaction.setUser(user);
+        toTransaction.setType(TransactionType.DEPOSIT);
+        toTransaction.setAccount(toAccount);
+        toTransaction.setCategory(category);
+        if(categoryId != 0) {toTransaction.setCategoryName(category.getName());}
+        toTransaction.setPreviousTotal(toTotal);
+        transactionDao.save(toTransaction);
+        accountDao.save(toAccount);
 
         model.addAttribute("title", "Transaction Successful");
         return "transaction/transaction-confirmation";
